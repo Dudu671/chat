38eb3f4c003c91d066733871a1f1b15d10e1f5cb
Matos <eduardoooax@gmail.com>
2021-03-30 18:57:21 -0300

    Main chat created.

--- src/pages/MainChat/index.js
+++ src/pages/MainChat/index.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { io } from 'socket.io-client'
 
 import './styles.css'
 import Message from '../../components/Message'
 
+const socket = io('http://192.168.0.189:3001')
+
 export default function MainChat() {
-    const mockMessages = [
-        { user: 'Dudu', text: 'Olá, boa noite', time: '04h00' },
-        { user: 'Guina', text: 'Boa noite', time: '04h00' },
-        { user: 'Dudu', text: 'Como vai?', time: '04h01' },
-        { user: 'Guina', text: 'Bem, e você?', time: '04h01' },
-        { user: 'Dudu', text: 'Bem', time: '04h01' }
-    ]
+    const [user, setUser] = useState(null)
+    const [messageToSend, setMessageToSend] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        const userPrompt = prompt('Choose a username to enter the chat room')
+        setUser(userPrompt)
+    }, [])
+
+
+    socket.on('chat message', msg => {
+        setMessages([...messages, { user: msg[0], text: msg[1], time: msg[2] }])
+    })
+
+    const sendMessage = () => {
+        if (user === null) return alert('You must choose a username to send messages')
+        const time = new Date()
+        socket.emit('chat message', [user, messageToSend, time])
+        setMessageToSend('')
+    }
 
     return (
         <div className="mainChatContainer">
             <div className="messages">
-                {mockMessages.map(message => (
+                {messages.map(message => (
                     <Message user={message.user} text={message.text} time={message.time} />
                 ))}
             </div>
 
             <div className="textareaContainer">
-                <textarea rows="3" placeholder="Type your message..." />
-                <button type="button">Enviar</button>
+                <textarea
+                    rows="3"
+                    placeholder="Type your message..."
+                    onChange={e => setMessageToSend(e.target.value)}
+                />
+
+                <button type="button" onClick={() => sendMessage()}>Enviar</button>
             </div>
         </div>
     )

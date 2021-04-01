63692c9bd76b1b43c834dd602a02b64b730780e6
Matos <eduardoooax@gmail.com>
2021-04-01 07:24:48 -0300

    Private Chat page created (in progress), React Router addead and navbar created.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { FaBars } from 'react-icons/fa'
 
 import './App.css'
 import MainChat from './pages/MainChat'
+import PrivateChat from './pages/PrivateChat'
 
 export default function App() {
+  const [showingPage, setShowingPage] = useState("MainChat")
+
+  const selectPage = () => {
+    if (showingPage === "MainChat") return <MainChat />
+    if (showingPage === "PrivateChat") return <PrivateChat />
+  }
+
+  const menu = () => {
+    const navCont = document.querySelector("#navCont");
+    navCont.classList.toggle("menuRightAni")
+  }
+
   return (
     <div className="container">
-      <MainChat />
+      <Router>
+        <header>
+          <label className="logo">Simple Chat</label>
+          <label className="checkBtn" onClick={() => menu()}>
+            <FaBars color="rgb(56, 192, 56)" />
+          </label>
+
+          <nav id="navCont">
+            <ul>
+              <li>
+                <Link to="/private-chat">Private chat</Link>
+              </li>
+              <li>
+                <Link to="/main-chat">Main chat</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route path="/private-chat">
+            <PrivateChat />
+          </Route>
+          <Route path="/main-chat">
+            <MainChat />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
---
+++ src/pages/PrivateChat/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PrivateChat() {
+    return (
+        <div>
+            <h1>Private Chat</h1>
+        </div>
+    )
+}

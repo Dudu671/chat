a54247d6437d3396b7d110d25f3ae54bc21bab25
Matos <eduardoooax@gmail.com>
2021-04-05 15:10:51 -0300

    Dotenv removed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
-import { FaBars } from 'react-icons/fa'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import './App.css'
 import MainChat from './pages/MainChat'
-import PrivateChat from './pages/PrivateChat'
 
 export default function App() {
-  const toggleDropdownMenu = () => {
-    const navContainer = document.querySelector("#navContainer");
-    navContainer.classList.toggle("bringMenuToRight")
-  }
-
   return (
     <div className="container">
       <Router>
         <header>
           <label className="logo">Simple Chat</label>
-          <label className="checkBtn" onClick={() => toggleDropdownMenu()}>
-            <FaBars color="rgb(56, 192, 56)" />
-          </label>
-
-          <nav id="navContainer">
-            <ul>
-              <li>
-                <Link to="/private-chat">Private chat</Link>
-              </li>
-              <li>
-                <Link to="/main-chat">Main chat</Link>
-              </li>
-            </ul>
-          </nav>
         </header>
 
         <Switch>
-          <Route path="/private-chat">
-            <PrivateChat />
-          </Route>
-          <Route path="/main-chat">
+          <Route path="/">
             <MainChat />
           </Route>
         </Switch>
--- src/pages/PrivateChat/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PrivateChat() {
-    return (
-        <div>
-            <h1>Private Chat</h1>
-        </div>
-    )
-}
